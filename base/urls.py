adc3e68ffe63409aea877b4c39f9f226eba843be
unknown <darrylandrew22@gmail.com>
2023-03-28 15:44:15 +0300

    new merge

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,6 @@ from base import views
 urlpatterns = [
     path('', views.home, name="main"),
     path('login/', views.login, name="login"),
-    path('signup/', views.signup, name="signup")
+    path('signup/', views.signup, name="signup"),
+    path('dashboard/', views.dashboard, name="dashboard"),
 ]
--- base/views.py
+++ base/views.py
@@ -10,3 +10,6 @@ def login(request):
 
 def signup(request):
     return render(request, 'signup.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
